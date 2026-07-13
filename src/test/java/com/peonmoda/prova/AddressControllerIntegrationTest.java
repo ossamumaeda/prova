e3700a4a84d3b8a4bdb588337eba00f1b9e910f5
ossamumaeda@gmail.com <ossamumaeda@gmail.com>
2026-07-13 12:51:30 -0300

    Fix - Remove debugg from address  Integration test

--- src/test/java/com/peonmoda/prova/AddressControllerIntegrationTest.java
+++ src/test/java/com/peonmoda/prova/AddressControllerIntegrationTest.java
@@ -13,8 +13,10 @@ import com.peonmoda.prova.factory.PersonFactory;
 import com.peonmoda.prova.repository.AddressRepository;
 import com.peonmoda.prova.repository.PersonRepository;
 
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -27,59 +29,85 @@ import org.springframework.test.context.DynamicPropertySource;
 
 import org.springframework.test.web.servlet.MockMvc;
 
+
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+
 import java.util.UUID;
+
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+
+
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
 class AddressControllerIntegrationTest {
 
+
     @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16-alpine")
-            .withDatabaseName("persondb")
-            .withUsername("person")
-            .withPassword("person");
+    static PostgreSQLContainer<?> postgres =
+            new PostgreSQLContainer<>("postgres:16-alpine")
+                    .withDatabaseName("persondb")
+                    .withUsername("person")
+                    .withPassword("person");
+
+
 
     @DynamicPropertySource
     static void configureDatabase(
             DynamicPropertyRegistry registry) {
 
+
         registry.add(
                 "spring.datasource.url",
-                postgres::getJdbcUrl);
+                postgres::getJdbcUrl
+        );
+
 
         registry.add(
                 "spring.datasource.username",
-                postgres::getUsername);
+                postgres::getUsername
+        );
+
 
         registry.add(
                 "spring.datasource.password",
-                postgres::getPassword);
+                postgres::getPassword
+        );
 
     }
+
+
 
     @Autowired
     private MockMvc mockMvc;
 
+
+
     @Autowired
     private ObjectMapper objectMapper;
+
+
 
     @Autowired
     private PersonRepository personRepository;
 
+
+
     @Autowired
     private AddressRepository addressRepository;
+
+
 
     @BeforeEach
     void cleanDatabase() {
@@ -89,58 +117,97 @@ class AddressControllerIntegrationTest {
 
     }
 
+
+
     private UUID createPerson() throws Exception {
 
-        CreatePersonRequest request = PersonFactory.createRequest();
 
-        String response = mockMvc.perform(post("/api/v1/pessoas")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                        objectMapper.writeValueAsString(request)))
+        CreatePersonRequest request =
+                PersonFactory.createRequest();
+
+
+
+        String response =
+                mockMvc.perform(post("/api/v1/pessoas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                objectMapper.writeValueAsString(request)
+                        ))
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        JsonNode json = objectMapper.readTree(response);
+
+
+        JsonNode json =
+                objectMapper.readTree(response);
+
+
 
         return UUID.fromString(
                 json.get("id")
-                        .asText());
+                        .asText()
+        );
 
     }
+
+
+
 
     private UUID createAddress(UUID personId)
             throws Exception {
 
-        CreateAddressRequest request = AddressFactory.createRequest();
 
-        String response = mockMvc.perform(post(
-                "/api/v1/pessoas/{personId}/enderecos",
-                personId)
+        CreateAddressRequest request =
+                AddressFactory.createRequest();
 
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(
-                        objectMapper.writeValueAsString(request)))
+
+
+        String response =
+                mockMvc.perform(post(
+                        "/api/v1/pessoas/{personId}/enderecos",
+                        personId)
+
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(
+                                objectMapper.writeValueAsString(request)
+                        ))
                 .andExpect(status().isCreated())
                 .andReturn()
                 .getResponse()
                 .getContentAsString();
 
-        JsonNode json = objectMapper.readTree(response);
+
+
+        JsonNode json =
+                objectMapper.readTree(response);
+
+
 
         return UUID.fromString(
                 json.get("id")
-                        .asText());
+                        .asText()
+        );
 
     }
+
+
+
+
 
     @Test
     void shouldCreateAddressSuccessfully()
             throws Exception {
 
-        UUID personId = createPerson();
 
-        CreateAddressRequest request = AddressFactory.createRequest();
+        UUID personId =
+                createPerson();
+
+
+        CreateAddressRequest request =
+                AddressFactory.createRequest();
+
+
 
         mockMvc.perform(post(
                 "/api/v1/pessoas/{personId}/enderecos",
@@ -149,54 +216,78 @@ class AddressControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
 
                 .content(
-                        objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.logradouro")
-                        .value(request.logradouro()));
+                    objectMapper.writeValueAsString(request)
+                ))
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("$.logradouro")
+                .value(request.logradouro()));
+
+
 
         assertEquals(
                 2,
-                addressRepository.count());
+                addressRepository.count()
+        );
 
     }
+
+
+
+
 
     @Test
     void shouldGetAddressById()
             throws Exception {
 
-        UUID personId = createPerson();
 
-        UUID addressId = createAddress(personId);
+        UUID personId =
+                createPerson();
+
+
+        UUID addressId =
+                createAddress(personId);
+
+
 
         mockMvc.perform(get(
                 "/api/v1/pessoas/{personId}/enderecos/{addressId}",
                 personId,
                 addressId))
 
-                .andExpect(status().isOk())
+        .andExpect(status().isOk())
 
-                .andExpect(jsonPath("$.id")
-                        .value(addressId.toString()));
+        .andExpect(jsonPath("$.id")
+                .value(addressId.toString()));
 
     }
+
+
+
+
 
     @Test
     void shouldListAddresses()
             throws Exception {
 
-        UUID personId = createPerson();
+
+        UUID personId =
+                createPerson();
+
+
 
         createAddress(personId);
         createAddress(personId);
+
+
 
         mockMvc.perform(get(
                 "/api/v1/pessoas/{personId}/enderecos",
                 personId))
 
-                .andExpect(status().isOk())
+        .andExpect(status().isOk())
 
-                .andExpect(jsonPath("$",
-                        hasSize(3)));
+        .andExpect(jsonPath("$",
+                hasSize(3)));
 
     }
 
@@ -204,11 +295,14 @@ class AddressControllerIntegrationTest {
     void shouldUpdateAddressSuccessfully()
             throws Exception {
 
-        UUID personId = createPerson();
+        UUID personId =
+                createPerson();
 
-        UUID addressId = createAddress(personId);
+        UUID addressId =
+                createAddress(personId);
 
-        UpdateAddressRequest request = AddressFactory.updateRequest(addressId);
+        UpdateAddressRequest request =
+                AddressFactory.updateRequest(addressId);
 
         mockMvc.perform(patch(
                 "/api/v1/pessoas/{personId}/enderecos/{addressId}",
@@ -218,11 +312,13 @@ class AddressControllerIntegrationTest {
                 .contentType(MediaType.APPLICATION_JSON)
 
                 .content(
-                        objectMapper.writeValueAsString(request)))
+                    objectMapper.writeValueAsString(request)
+                ))
 
-                .andDo(print())
+        .andExpect(status().isOk())
 
-                .andExpect(status().isOk());
+        .andExpect(jsonPath("$.logradouro")
+                .value(request.logradouro()));
 
     }
 
@@ -230,50 +326,74 @@ class AddressControllerIntegrationTest {
     void shouldDeleteAddressSuccessfully()
             throws Exception {
 
-        UUID personId = createPerson();
 
-        UUID addressId = createAddress(personId);
+        UUID personId =
+                createPerson();
+
+
+
+        UUID addressId =
+                createAddress(personId);
+
+
 
         mockMvc.perform(delete(
                 "/api/v1/pessoas/{personId}/enderecos/{addressId}",
                 personId,
                 addressId))
 
-                .andExpect(status().isNoContent());
+        .andExpect(status().isNoContent());
+
+
 
         assertEquals(
                 false,
                 addressRepository.findById(addressId)
                         .get()
-                        .getAtivo());
+                        .getAtivo()
+        );
 
     }
+
+
+
+
 
     @Test
     void shouldReturnNotFoundWhenPersonDoesNotExist()
             throws Exception {
 
+
         mockMvc.perform(get(
                 "/api/v1/pessoas/{personId}/enderecos",
                 UUID.randomUUID()))
 
-                .andExpect(status().isNotFound());
+        .andExpect(status().isNotFound());
 
     }
+
+
+
+
 
     @Test
     void shouldReturnNotFoundWhenAddressDoesNotExist()
             throws Exception {
 
-        UUID personId = createPerson();
+
+        UUID personId =
+                createPerson();
+
+
 
         mockMvc.perform(get(
                 "/api/v1/pessoas/{personId}/enderecos/{addressId}",
                 personId,
                 UUID.randomUUID()))
 
-                .andExpect(status().isNotFound());
+        .andExpect(status().isNotFound());
 
     }
+
 
 }
